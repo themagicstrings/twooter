1adf27205ec66d5f713c75193517cf1fa806b73b
joglr <1959615+joglr@users.noreply.github.com>
2021-02-06 13:10:04 +0100

    Fix pip3 name

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-sudo apt install gcc python3 libsqlite3-dev sqlitebrowser sqlite3 pip3
+sudo apt install gcc python3 libsqlite3-dev sqlitebrowser sqlite3 python-pip3
 pip3 install flask
 
 
