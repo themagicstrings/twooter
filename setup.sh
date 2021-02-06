85bdb90be76a69c80cf25940ef880c9d9df4ceab
Jonas Røssum <1959615+joglr@users.noreply.github.com>
2021-02-06 16:01:28 +0100

    Add updating to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
-sudo apt install gcc python3 libsqlite3-dev sqlitebrowser sqlite3 python3-pip
+# Update apt-get
+apt-get update
+
+# Update apt
+apt update
+
+apt install gcc python3 libsqlite3-dev sqlitebrowser sqlite3 python3-pip
 pip3 install flask
-# Allows all to execute control.sh 
+# Allows all to execute control.sh
 chmod a+x ./src/control.sh
-
-
-
-
-
-
-
