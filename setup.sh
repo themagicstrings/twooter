02739f18e0f9b902b3bb26ebb69f8c4506fdeb0e
hojelse <info@hojelse.com>
2021-02-06 15:34:23 +0100

    Update chmod comment

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 sudo apt install gcc python3 libsqlite3-dev sqlitebrowser sqlite3 python3-pip
 pip3 install flask
-#Makes control.sh an executable
+# Allows all to execute control.sh 
 chmod a+x ./src/control.sh
 
 
