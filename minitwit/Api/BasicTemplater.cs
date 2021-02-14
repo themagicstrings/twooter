9f8a37fafe378e2ad25d8725e00b90806d5362b2
emiljapelt <55390848+emiljapelt@users.noreply.github.com>
2021-02-14 19:51:22 +0100

    Work on follow/unfollow

--- minitwit/Api/BasicTemplater.cs
+++ minitwit/Api/BasicTemplater.cs
@@ -8,15 +8,19 @@ namespace Api
 {
     public class BasicTemplater
     {
-        public static string GenerateTimeline(List<MessageReadDTO> messages, bool postable)
+        public static string GenerateTimeline(List<MessageReadDTO> messages, bool loggedin)
         {
             messages.Sort((x, y) => DateTime.Compare(x.pub_date, y.pub_date));
             messages.Reverse();
 
             StringBuilder sb = new StringBuilder();
             sb.Append("<html>");
-            if(postable) sb.Append("<form method=post action=add_message><input name=Text><input type=submit></form>");
-            foreach(MessageReadDTO msg in messages) sb.Append($"<p>{msg.author.username} [{msg.pub_date}]: {msg.text}</p>");
+            if(loggedin) sb.Append("<form method=post action=add_message><input name=Text><input type=submit></form>");
+            foreach(MessageReadDTO msg in messages) 
+            {
+                sb.Append($"<p>{msg.author.username} [{msg.pub_date}]: {msg.text}</p>");
+                if(loggedin) sb.Append($"<form method=post action={msg.author.username}/follow><button type=submit>Follow</button></form>");
+            }
             sb.Append("</html>");
             return sb.ToString();
         }
