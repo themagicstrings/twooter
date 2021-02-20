77e40891eb674d7330d9f2ad8ef61a236c488826
Jonas Røssum <1959615+joglr@users.noreply.github.com>
2021-02-20 13:29:00 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,5 +11,11 @@ sudo apt-get update
 sudo apt-get install -y apt-transport-https
 sudo apt-get update
 sudo apt-get install -y dotnet-sdk-5.0
-
+rm packages-microsoft-prod.deb
 sudo apt install python-pytest
+
+sudo apt-get install -y virtualbox virtualbox-ext-pack
+wget -c https://releases.hashicorp.com/vagrant/2.2.14/vagrant_2.2.14_x86_64.deb
+sudo dpkg -i vagrant_2.2.14_x86_64.deb
+rm vagrant_2.2.14_x86_64.deb
+vagrant plugin install vagrant-digitalocean
