c20a86ca437944bcf97d6d2382301e6cce2a0471
Jonas Røssum <1959615+joglr@users.noreply.github.com>
2021-02-20 13:33:28 +0100

    Add warning if running without root permission

--- setup.sh
+++ setup.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please use sudo"
+  exit
+fi
+
+
 # Update apt-get
 apt-get update
+apt-update
 
 # Setup .NET 5
 
 wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 dpkg -i packages-microsoft-prod.deb
+apt-get install -y apt-transport-https
 
-sudo apt-get update
-sudo apt-get install -y apt-transport-https
-sudo apt-get update
-sudo apt-get install -y dotnet-sdk-5.0
+apt-get install -y dotnet-sdk-5.0
 rm packages-microsoft-prod.deb
-sudo apt install python-pytest
+apt install python-pytest
 
-sudo apt-get install -y virtualbox virtualbox-ext-pack
+# Vagrant
+apt-get install -y virtualbox virtualbox-ext-pack
 wget -c https://releases.hashicorp.com/vagrant/2.2.14/vagrant_2.2.14_x86_64.deb
-sudo dpkg -i vagrant_2.2.14_x86_64.deb
+dpkg -i vagrant_2.2.14_x86_64.deb
 rm vagrant_2.2.14_x86_64.deb
 vagrant plugin install vagrant-digitalocean
