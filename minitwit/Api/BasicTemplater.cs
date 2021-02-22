c80a205b5e525c7e816c965e4ac0543d3022a51b
joglr <1959615+joglr@users.noreply.github.com>
2021-02-22 11:43:11 +0100

    Fix indentation

--- minitwit/Api/BasicTemplater.cs
+++ minitwit/Api/BasicTemplater.cs
@@ -158,8 +158,7 @@ namespace Api
 
     internal static string Generate404Page(UserReadDTO user)
     {
-      return @"
-      <!DOCTYPE HTML PUBLIC ""-//W3C//DTD HTML 3.2 Final//EN"">
+      return @"<!DOCTYPE HTML PUBLIC ""-//W3C//DTD HTML 3.2 Final//EN"">
 <title>404 Not Found</title>
 <h1>Not Found</h1>
 <p>The requested URL was not found on the server. If you entered the URL manually please check your spelling and try again.</p>
