b05d1ed07ea6ee24f99b1d35d610fb3f8627f623
joglr <1959615+joglr@users.noreply.github.com>
2021-03-08 11:20:37 +0100

    Update setup_server

--- setup_server.sh
+++ setup_server.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 docker network create twooter-network
-docker run --name dbserver -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=$DB_PASSWORD" -d --net twooter-network -p 1433:1433 mcr.microsoft.com/mssql/server:2019-latest
-77286670-16bd-4026-b4af-7d7b70b6e0e1
+docker run -h dbserver --name dbserver -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=$DB_PASSWORD" -d --net twooter-network -p 1433:1433 mcr.microsoft.com/mssql/server:2019-latest
