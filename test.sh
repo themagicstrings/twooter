ad23f929ee5f333666da6e314f02a1ddfe676c67
joglr <1959615+joglr@users.noreply.github.com>
2021-03-08 09:28:23 +0100

    Record exit codes and exit properly in test.sh

--- test.sh
+++ test.sh
@@ -1,24 +1,35 @@
 #!/bin/bash
 
-#Run model tests
+# Run model tests
 cd minitwit/Models.Test
 dotnet test
+exitCode1=$?
+testid=$!
 
-#Start the api in background
+
+# Start the api in background
 cd ..
 cd Api
-dotnet run &
-sleep 5s
+nohup dotnet run > /dev/null &
 apipid=$!
+sleep 5s
 
-#Run api tests
+# Run api tests
 cd ..
 cd Api.Test
 pip3 install pytest
 python3 -m pytest SimulationControllerTests.py
+exitCode2=$?
 python3 -m pytest MinitwitControllerTests.py
+exitCode3=$?
 
-#Stop the api
+# Stop the api
 kill $apipid
 
-$SHELL
+if [ $exitCode1 -ne 0 -o $exitCode2 -ne 0 -o $exitCode3 -ne 0 ] ; then
+    echo "Tests failed"
+    exit 1
+  else
+    echo "Tests passed"
+    exit 0
+fi
