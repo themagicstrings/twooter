72a553c88fe2e5ff4397b7753c749c15657001e1
Jonas Røssum <1959615+joglr@users.noreply.github.com>
2021-03-12 14:57:18 +0100

    Add prometheus setup

--- setup_server.sh
+++ setup_server.sh
@@ -1,3 +1,60 @@
 #!/bin/bash
-docker network create twooter-network
-docker run -h dbserver --name dbserver -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=$DB_PASSWORD" -d --net twooter-network -p 1433:1433 mcr.microsoft.com/mssql/server:2019-latest
+
+sudo apt update
+sudo apt install nginx
+sudo systemctl start nginx
+sudo useradd --no-create-home --shell /bin/false prome
+sudo useradd --no-create-home --shell /bin/false node_exporter
+sudo mkdir /etc/prometheus
+sudo mkdir /var/lib/prometheus
+sudo wget https://github.com/prometheus/prometheus/releases/download/v2.0.0/
+sudo prometheus-2.0.0.linux-amd64.tar.gz
+sudo tar xvf prometheus-2.0.0.linux-amd64.tar.gz
+sudo cp prometheus-2.0.0.linux-amd64/prometheus /usr/local/bin/
+sudo cp prometheus-2.0.0.linux-amd64/promtool /usr/local/bin/
+sudo chown prome:prome /usr/local/bin/prometheus
+sudo chown prome:prome /usr/local/bin/promtool
+sudo cp -r prometheus-2.0.0.linux-amd64/consoles /etc/prometheus
+sudo cp -r prometheus-2.0.0.linux-amd64/console_libraries /etc/prometheus
+sudo chown -R prome:prome /etc/prometheus/consoles
+sudo chown -R prome:prome /etc/prometheus/console_libraries
+
+sudo cat > /etc/prometheus/prometheus.yml <<- EOM
+global:
+  scrape_interval: 15s
+
+scrape_configs:
+  - job_name: 'prometheus'
+    scrape_interval: 5s
+    static_configs:
+      - targets: ['localhost:80']
+remote_write:
+- url: https://prometheus-us-central1.grafana.net/api/prom/push
+  basic_auth:
+    username: 58823
+    password: $GRAFANA_PASSWORD
+EOM
+
+sudo cat > /etc/systemd/system/prometheus.service <<- EOM
+[Unit]
+Description=Prometheus
+Wants=network-online.target
+After=network-online.target
+
+[Service]
+User=prome
+Group=prome
+Type=simple
+ExecStart=/usr/local/bin/prometheus \
+    --config.file /etc/prometheus/prometheus.yml \
+    --storage.tsdb.path /var/lib/prometheus/ \
+    --web.console.templates=/etc/prometheus/consoles \
+    --web.console.libraries=/etc/prometheus/console_libraries
+
+[Install]
+WantedBy=multi-user.target
+EOM
+sudo systemctl daemon-reload
+sudo systemctl start prometheus
+sudo systemctl enable prometheus
+sudo systemctl status prometheus
