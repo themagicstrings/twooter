497a4fdc87f28acf3be3a002b851ad392c232227
Jonas Glerup Røssum <1959615+joglr@users.noreply.github.com>
2021-03-12 14:05:50 +0100

    Add prometheus monitoring (#29)

--- minitwit/Api/Startup.cs
+++ minitwit/Api/Startup.cs
@@ -1,23 +1,16 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Entities;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using Models;
+using Prometheus;
 
 namespace Api
 {
-    public class Startup
+  public class Startup
     {
         public Startup(IConfiguration configuration)
         {
@@ -71,12 +64,15 @@ namespace Api
 
             app.UseRouting();
 
+            app.UseHttpMetrics();
+
             app.UseSession();
 
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapMetrics();
                 endpoints.MapControllers();
             });
         }
